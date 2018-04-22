b80ed21dca367d6f6ecc0ed9dbdd5a155098f988
techmoe <i@lolicookie.com>
2018-04-22 23:07:08 +0800

    fix

--- devscript/build_pyinstaller_windows.sh
+++ devscript/build_pyinstaller_windows.sh
@@ -5,11 +5,16 @@
 echo "Install dependencies"
 pip install -e .
 echo "Build windows executable files"
+
+# pyinstaller doesn't support to use the wildcard in CLI argument `--hidden-import`, 
+# I will try to fix this problem in next version
+
 pyinstaller -F ${BUILD_APP_ENTRY} \
             --distpath pyinstaller/dist \
             --specpath pyinstaller/spec \
             --workpath pyinstaller/build \
-            --hidden-import="dcdownloader.parser.*"
+            --hidden-import="dcdownloader.parser.DmzjParser" \
+            --hidden-import="dcdownloader.parser.EhentaiParser"
 
 echo "Rename output file"
 mv pyinstaller/dist/main.exe  pyinstaller/dist/${BUILD_OUTPUT_FILE_NAME}
